601b68b94ba5ed74f711fa9025c75b0d29877e17
Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>
2024-03-13 00:41:14 +0500

    Add files via upload
    Signed-off-by: Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>

---
+++ JS practice table/app.js
@@ -0,0 +1,53 @@
+let nam='<center>Name: Siddiqa Badar'
+let roll='<br><center> Roll no.: JS1-053'
+let teacher='<br><center> Teacher Name: Miss Shafaque Anees <br><br>'
+document.write(nam + roll + teacher);
+
+let tableA= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableA+'X'+i+'='+tableA*i,'<br><br>')
+}
+
+let tableB= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableB+'X'+i+'='+tableB*i,'<br><br>')
+}
+
+
+let tableC= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableC+'X'+i+'='+tableC*i,'<br><br>')
+}
+
+
+
+
+
+let tableD= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableD+'X'+i+'='+tableD*i,'<br><br>')
+}
+
+
+
+let tableE= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableE+'X'+i+'='+tableE*i,'<br><br>')
+}
+
+
+
+let tableF= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableF+'X'+i+'='+tableF*i,'<br><br>')
+}
+let tableG= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableG+'X'+i+'='+tableG*i,'<br><br>')
+}
+let tableH= +prompt('Enter number');
+for(let i=1; i<=10; i++){
+document.write(tableH+'X'+i+'='+tableH*i,'<br><br>')
+}
+
+
