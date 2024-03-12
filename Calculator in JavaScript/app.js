0b3a3428364a1692cd9f505499146702b35fcc86
Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>
2024-03-12 23:37:21 +0500

    Add files via upload
    Signed-off-by: Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>

---
+++ Calculator in JavaScript/app.js
@@ -0,0 +1,18 @@
+
+function press(e) {
+    document.getElementById('inp').value +=e
+}
+
+function eq() {
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+
+function clr(){
+    document.getElementById('inp').value =''
+}
+
+function clr1(){
+    var a = document.getElementById('inp')
+    a.value = a.value.slice (0,-1)
+}
